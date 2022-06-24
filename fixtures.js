028f85eda9c08821b9902a6b712a4221e7a081e8
DevWeb13 <giuliano.loic@sfr.fr>
2022-06-24 12:44:39 +0200

    feat: (fixtures.js): For create list of employees

---
+++ fixtures.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const Employee = require('./models/Employee');
+const data = require('./MOCK_DATA.js');
+
+
+// Replace process.env.MONGODB_URI with YOUR mongodb url !!!!!
+const cleAPI = process.env.MONGODB_URI || '';
+
+async function start() {
+  /* Connecting to the MongoDB database. */
+  try {
+    await mongoose.connect(cleAPI,
+      {
+        // @ts-ignore
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+    console.log('Connexion à MongoDB réussie !');
+    for (const fixture in data) {
+      console.log(fixture);
+      const employee = new Employee(data[fixture]);
+      await employee.save();
+      console.log('Employee added successfully')
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+start();
